5bcf95adb0565a0a902c0cb4a0e0f5aafb2d208a
andtv <tvbanw@gmail.com>
2023-02-09 18:10:16 +0100

    1

--- nexus/plugin.program.tvban-Nexus/resources/libs/gui/build_menu.py
+++ nexus/plugin.program.tvban-Nexus/resources/libs/gui/build_menu.py
@@ -47,7 +47,7 @@ class BuildMenu:
                     directory.add_dir('[COLOR aliceblue][/COLOR] [B][COLOR azure]•[/COLOR][COLOR yellowgreen]{0} (v{1})[/COLOR][/B]'.format(name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME3)
                     directory.add_separator()
                 if float(kodi) == 20.0:
-                    directory.add_dir('[COLOR aliceblue][/COLOR] [B][COLOR azure]•[/COLOR][COLOR yellowgreen]{0} (v{1})[/COLOR][/B]'.format(name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME3)
+                    directory.add_dir('[COLOR aliceblue][/COLOR] [B][COLOR azure]•[/COLOR][COLOR yellowgreen]{0} - v{1}[/COLOR][/B]'.format(name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME3)
 #                    directory.add_separator()
 #               elif float(kodi) == 19.0:
 #                    directory.add_dir('[COLOR aliceblue][/COLOR] [B][COLOR azure]•[/COLOR][COLOR yellowgreen]{0} (v{1})[/COLOR][/B]'.format(name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME3)
