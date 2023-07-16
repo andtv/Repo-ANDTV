6e8df2e878d2f341ce9b2261733d9247c57e01f2
andtv <tvbanw@gmail.com>
2023-07-16 17:42:42 +0200

    36

--- nexus/plugin.program.tvban-Nexus/resources/libs/gui/build_menu.py
+++ nexus/plugin.program.tvban-Nexus/resources/libs/gui/build_menu.py
@@ -43,7 +43,7 @@ class BuildMenu:
 
             if not kodiv or kodiv == int(float(kodi)):
                 menu = self.create_install_menu(name)
-                directory.add_dir('[{0}] {1} (v{2})'.format(float(kodi), name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME2)
+                directory.add_dir('[COLOR aliceblue][/COLOR] [B][COLOR azure]•[/COLOR][COLOR yellowgreen]{0} (v{1})[/COLOR][/B]'.format(float(kodi), name, version), {'mode': 'viewbuild', 'name': name}, description=description, fanart=fanart, icon=icon, menu=menu, themeit=CONFIG.THEME2)
 
     def theme_count(self, name, count=True):
         from resources.libs import check
